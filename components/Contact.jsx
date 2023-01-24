f8f1f400747041a619917a79e7308b7d2fc3f345
Matias Arias <matt.arias182@gmail.com>
2023-01-24 17:19:17 -0300

    added Contact component

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -13,6 +13,49 @@ const Contact = () => {
         <h3 className="text-sky-400 text-xl md:text-2xl font-bold mt-8">
           Let's Connect
         </h3>
+
+        <div className="w-full max-w-screen-sm mx-auto">
+          <form
+            onSubmit=""
+            action=""
+            className="flex flex-col justify-center items-center gap-6"
+          >
+            <input
+              type="text"
+              className="w-full py-2 px-4 bg-indigo-200/50 text-gray-200 text-base lg:text-lg  rounded-xl placeholder:text-gray-200 focus:outline-none focus:shadow-lg focus:shadow-sky-500"
+              placeholder="Name"
+              name=""
+            />
+
+            <input
+              type="email"
+              className="w-full py-2 px-4 bg-indigo-200/50 text-gray-200 text-base lg:text-lg  rounded-xl placeholder:text-gray-200 focus:outline-none focus:shadow-lg focus:shadow-sky-500"
+              placeholder="Email"
+              name=""
+            />
+
+            <input
+              type="text"
+              className="w-full py-2 px-4 bg-indigo-200/50 text-gray-200 text-base lg:text-lg  rounded-xl placeholder:text-gray-200 focus:outline-none focus:shadow-lg focus:shadow-sky-500"
+              placeholder="Subject"
+              name=""
+            />
+
+            <textarea
+              className="w-full py-2 px-4 bg-indigo-200/50 text-gray-200 text-base lg:text-lg rounded-xl placeholder:text-gray-200 focus:outline-none focus:shadow-lg focus:shadow-sky-500 resize-none"
+              placeholder="Message"
+              rows="8"
+              name=""
+            ></textarea>
+
+            <button
+              type="submit"
+              className="w-full py-2 bg-yellow-500/50 text-gray-200 font-bold rounded-xl hover:bg-yellow-400/70"
+            >
+              Send Message
+            </button>
+          </form>
+        </div>
       </div>
     </section>
   );
