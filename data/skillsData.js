fac33778c88bb9cee647a8ad271b5b4ba4668394
Matias Arias <matt.arias182@gmail.com>
2023-08-27 00:48:04 -0300

    added skills images png

--- data/skillsData.js
+++ data/skillsData.js
@@ -1,85 +1,97 @@
 export const skills = [
   {
     id: 1,
-    title: "HTML",
-    image: "/assets/skills/html.png",
+    title: 'HTML',
+    image: '/assets/skills/html.png',
   },
 
   {
     id: 2,
-    title: "CSS",
-    image: "/assets/skills/css.png",
+    title: 'CSS',
+    image: '/assets/skills/css.png',
   },
 
   {
     id: 3,
-    title: "Bootstrap",
-    image: "/assets/skills/bootstrap.png",
+    title: 'Bootstrap',
+    image: '/assets/skills/bootstrap.png',
   },
 
   {
     id: 4,
-    title: "Tailwind CSS",
-    image: "/assets/skills/tailwind.png",
+    title: 'Tailwind CSS',
+    image: '/assets/skills/tailwind.png',
   },
 
   {
     id: 5,
-    title: "Material UI",
-    image: "/assets/skills/material-ui.png",
+    title: 'Material UI',
+    image: '/assets/skills/material-ui.png',
   },
 
   {
     id: 6,
-    title: "JavaScript",
-    image: "/assets/skills/javascript.png",
+    title: 'Styled Components',
+    image: '/assets/skills/styled-components.png',
   },
 
   {
     id: 7,
-    title: "React",
-    image: "/assets/skills/react.png",
+    title: 'JavaScript',
+    image: '/assets/skills/javascript.png',
   },
 
   {
     id: 8,
-    title: "Redux Toolkit",
-    image: "/assets/skills/redux.png",
+    title: 'TypeScript',
+    image: '/assets/skills/typescript.png',
   },
 
   {
     id: 9,
-    title: "Next.JS",
-    image: "/assets/skills/nextjs.png",
+    title: 'React',
+    image: '/assets/skills/react.png',
   },
 
   {
     id: 10,
-    title: "Node.js",
-    image: "/assets/skills/node.png",
+    title: 'Redux Toolkit',
+    image: '/assets/skills/redux.png',
   },
 
   {
     id: 11,
-    title: "Express.js",
-    image: "/assets/skills/express.js.png",
+    title: 'Next.JS',
+    image: '/assets/skills/nextjs.png',
   },
 
   {
     id: 12,
-    title: "MongoDB",
-    image: "/assets/skills/mongo.png",
+    title: 'Node.js',
+    image: '/assets/skills/node.png',
   },
 
   {
     id: 13,
-    title: "Firebase",
-    image: "/assets/skills/firebase.png",
+    title: 'Express.js',
+    image: '/assets/skills/express.js.png',
   },
 
   {
     id: 14,
-    title: "Github",
-    image: "/assets/skills/github.png",
+    title: 'MongoDB',
+    image: '/assets/skills/mongo.png',
+  },
+
+  {
+    id: 15,
+    title: 'Firebase',
+    image: '/assets/skills/firebase.png',
+  },
+
+  {
+    id: 16,
+    title: 'Github',
+    image: '/assets/skills/github.png',
   },
 ];
